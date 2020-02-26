4cda38afe45189f24f0423ee46d02e0c0aba376e
pjdevs <pj33720@gmail.com>
2020-02-26 19:35:25 +0100

    Reorganize intent events

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ server.use(parser.json());
 server.use(morgan('common'));
 
 server.get('/', (req, res) => {
-    res.send('Only url supported is post on /');
+    res.send('Only post is supported on /');
 });
 
 server.post('/', (req, res) => {
@@ -62,50 +62,56 @@ server.post('/', (req, res) => {
 
     res.setHeader('Content-Type', 'application/json');
 
-    if (intent == 'Prochain cours') {
-        infos.getNextCourse()
-            .then(course => {
-                res.send(simpleResponse('Vous avez ' + course.name + course.start, false));
-            })
-            .catch(() => {
-                res.status(501).send(simpleResponse('Erreur pendant la requète à l\'emploi du temps, réessayez plus tard', true));
-            });
-    }
-    else if (intent == "Heure de fin") {
-        let date = new Date(req.body.queryResult.outputContexts[0].parameters.date);
-        
-        infos.getEndTime(date)
-            .then(course => {
-                if (course == undefined) res.send(simpleResponse('Vous n\'avez aucun cours ce jour la'));
-                else res.send(simpleResponse('Vous finissez à ' + course.end + ' avec ' + course.name));
-            })
-            .catch(() => {
-                res.status(501).send(simpleResponse('Erreur pendant la requète à l\'emploi du temps, réessayez plus tard', true));
-            });
-    }
-    else if (intent == "Liste de cours") {
-        let date = new Date(req.body.queryResult.outputContexts[0].parameters.date);
-
-        infos.getDayCourse(date)
-            .then(courses => {
-                if (courses.length == 0) res.send(simpleResponse('Vous n\'avez aucun cours ce jour là'));
-                else {
-                    let text = '';
-
-                    courses.forEach(course => {
-                        text += `**${course.name}** _${course.schedule}_ : ${course.staff} ${course.room}  \n`;
-                    });
-
-                    res.send(basicCard('Vos cours', infos.getPrettyDate(date), text, 'Voilà vos cours !', false));
-                }
-            })
-            .catch(() => {
-                res.status(501).send(simpleResponse('Erreur pendant la requète à l\'emploi du temps, réessayez plus tard', true));
-            });
-    }
+    if (intent == 'Prochain cours') server.emit('next_course', req, res);
+    else if (intent == "Heure de fin") server.emit('end_time', req, res);
+    else if (intent == "Liste de cours") server.emit('course_list', req, res);
     else {
         res.send(simpleResponse('Je n\'ai pas reconnu votre demande, reessayez', false));
     }
+});
+
+server.on('next_course', (req, res) => {
+    infos.getNextCourse()
+    .then(course => {
+        res.send(simpleResponse('Vous avez ' + course.name + course.start, false));
+    })
+    .catch(() => {
+        res.status(501).send(simpleResponse('Erreur pendant la requète à l\'emploi du temps, réessayez plus tard', true));
+    });
+});
+
+server.on('end_time', (req, res) => {
+    let date = new Date(req.body.queryResult.outputContexts[0].parameters.date);
+        
+    infos.getEndTime(date)
+        .then(course => {
+            if (course == undefined) res.send(simpleResponse('Vous n\'avez aucun cours ce jour la'));
+            else res.send(simpleResponse('Vous finissez à ' + course.end + ' avec ' + course.name));
+        })
+        .catch(() => {
+            res.status(501).send(simpleResponse('Erreur pendant la requète à l\'emploi du temps, réessayez plus tard', true));
+        });
+});
+
+server.on('course_list', (req, res) => {
+    let date = new Date(req.body.queryResult.outputContexts[0].parameters.date);
+
+    infos.getDayCourse(date)
+        .then(courses => {
+            if (courses.length == 0) res.send(simpleResponse('Vous n\'avez aucun cours ce jour là'));
+            else {
+                let text = '';
+
+                courses.forEach(course => {
+                    text += `**${course.name}** _${course.schedule}_ : ${course.staff} ${course.room}  \n`;
+                });
+
+                res.send(basicCard('Vos cours', infos.getPrettyDate(date), text, 'Voilà vos cours !', false));
+            }
+        })
+        .catch(() => {
+            res.status(501).send(simpleResponse('Erreur pendant la requète à l\'emploi du temps, réessayez plus tard', true));
+        });
 });
 
 server.listen(80, '0.0.0.0', () => {
